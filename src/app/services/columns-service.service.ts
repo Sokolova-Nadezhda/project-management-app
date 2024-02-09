414f00f71c87d46ea2d7fe7ec90d2c1608fb73bf
Sokolova-Nadezhda <110903555+Sokolova-Nadezhda@users.noreply.github.com>
2024-02-09 17:16:17 +0200

    Update columns-service.service.ts

--- src/app/services/columns-service.service.ts
+++ src/app/services/columns-service.service.ts
@@ -23,14 +23,14 @@ export class ColumnsServiceService {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`,
     });
-    return this.http.post(serviceLink + ':3000/boards/' + boardId + '/columns', body, { headers });
+    return this.http.post(serviceLink + '/boards/' + boardId + '/columns', body, { headers });
   }
 
   removeColumn(idColumn: string, boardId: string) {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`,
     });
-    return this.http.delete(serviceLink + ':3000/boards/' + boardId + '/columns/' + idColumn, { headers });
+    return this.http.delete(serviceLink + '/boards/' + boardId + '/columns/' + idColumn, { headers });
   }
 
   updateColumn(idColumn: string, boardId: string, newParams: NewTitleForColumn) {
@@ -38,14 +38,14 @@ export class ColumnsServiceService {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`,
     });
-    return this.http.put(serviceLink + ':3000/boards/' + boardId + '/columns/' + idColumn, body, { headers });
+    return this.http.put(serviceLink + '/boards/' + boardId + '/columns/' + idColumn, body, { headers });
   }
 
   getColumnsByUserId() {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`,
     });
-    return this.http.get(serviceLink + ':3000/columnsSet?userId=' + this.userId, { headers });
+    return this.http.get(serviceLink + '/columnsSet?userId=' + this.userId, { headers });
   }
 
   updateSetOfColumns(arrayOfNewParams: NewOrdersOfColumns[]) {
@@ -53,7 +53,7 @@ export class ColumnsServiceService {
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.token}`,
     });
-    return this.http.patch(serviceLink + ':3000/columnsSet', body, { headers });
+    return this.http.patch(serviceLink + '/columnsSet', body, { headers });
   }
 }
 
